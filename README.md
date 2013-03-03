95dac73ddd1ce9a873c85cfcb259ab71b0e5a8f8
jldupont <github@jldupont.com>
2013-03-03 10:13:42 -0500

    more more README

--- README.md
+++ README.md
@@ -2,6 +2,18 @@ For more information, visit http://www.systemical.com/doc/opensource/pysubpub
 
 
 
+Small Example
+=============
+
+    from subpub import sub, pub
+    
+    @sub
+    def on_topic1(param1):
+        print "topic1: ", param1
+        
+    pub("topic", "value1")
+
+
 
 
 History
--- setup.py
+++ setup.py
@@ -16,7 +16,18 @@ DESC="""
 Overview
 --------
 
-This package offers a "publish-subscribe"
+This package offers a "publish-subscribe" framework.
+
+Small Example
+=============
+
+    from subpub import sub, pub
+    
+    @sub
+    def on_topic1(param1):
+        print "topic1: ", param1
+        
+    pub("topic", "value1")
 
 
 """
@@ -26,13 +37,19 @@ setup(name=         'pysubpub',
       description=  'Publish-Subscribe framework',
       author=       __author__,
       author_email= 'jl@jldupont.com',
-      url=          'http://www.systemical.com/doc/opensource/pysubpub',
+      url=          'https://github.com/jldupont/pysubpub',
       package_dir=  {'': "src",},
       packages=     find_packages("src"),
       zip_safe=False
       ,install_requires=[
                          ]
       ,long_description=DESC
+      ,classifiers=[
+                    'Development Status :: 4 - Beta',
+                    'Programming Language :: Python',
+                    'Intended Audience :: Developers',
+                    'Topic :: Utilities',
+                    ]
       )
 
 #############################################
